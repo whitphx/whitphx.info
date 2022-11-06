a5e80db7b2bcbd9108fa4cfcc7d565c393a9aae6
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-11-06 17:36:36 +0900

    Fix gtag IDs (#21)

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,8 +69,8 @@ module.exports = {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
           "G-XH47KLVQH4", // Google Analytics / GA
-          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
-          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+          // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
         // // This object gets passed directly to the gtag config command
         // // This config will be shared across all trackingIds
