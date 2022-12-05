da86377c7c059ca54ea53e146a98588e649a0f4b
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2022-12-05 21:34:39 +0900

    Set up gatsby-plugin-sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -196,6 +196,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
