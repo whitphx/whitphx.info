82ee5a317c3af983f99d15eafc95985b2ebd7c2a
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-01-05 23:25:55 +0900

    Set up ESLint for GraphQL following 'https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/#graphql-eslint'

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,44 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  overrides: [],
-  parser: "@typescript-eslint/parser",
+  root: true,
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
   rules: {},
+  // Ref: https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/#graphql-eslint
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      processor: '@graphql-eslint/graphql',
+      parser: "@typescript-eslint/parser",
+      plugins: ["react", "@typescript-eslint"],
+      extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+      ],
+      env: {
+        browser: true,
+        es2021: true,
+      },
+    },
+    {
+      files: ['*.graphql'],
+      parser: '@graphql-eslint/eslint-plugin',
+      plugins: ['@graphql-eslint'],
+      rules: {
+        '@graphql-eslint/no-anonymous-operations': 'error',
+        '@graphql-eslint/naming-convention': [
+          'error',
+          {
+            OperationDefinition: {
+              style: 'PascalCase',
+              forbiddenPrefixes: ['Query', 'Mutation', 'Subscription', 'Get'],
+              forbiddenSuffixes: ['Query', 'Mutation', 'Subscription'],
+            },
+          },
+        ],
+      },
+    },
+  ],
 }
