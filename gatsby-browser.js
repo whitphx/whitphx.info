8b3b03d2f8ce522f951739cc9d67e6209c937a09
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-01-06 17:26:10 +0900

    Remove unused typefaces

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 // custom typefaces
-import "typeface-montserrat";
-import "typeface-merriweather";
+// import "typeface-montserrat";  //  To use this, install it `yarn add `typeface-montserrat`
+// import "typeface-merriweather";  // To use this, install it `yarn add typeface-merriweather`.
 // normalize CSS across browsers
 import "./src/normalize.css";
 // custom CSS styles
