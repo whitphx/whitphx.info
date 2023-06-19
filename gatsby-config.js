e09b9df22329d17dab400b2a7c189f0efa2b46db
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2023-06-20 00:55:28 +0900

    Fix gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,26 +34,19 @@ module.exports = {
       options: {
         plugins: [
           {
-            resolve: `gatsby-transformer-remark`,
+            resolve: `@raae/gatsby-remark-oembed`,
             options: {
-              plugins: [
-                {
-                  resolve: `@raae/gatsby-remark-oembed`,
-                  options: {
-                    providers: {
-                      // Important to exclude providers
-                      // that adds js to the page.
-                      // If you do not need them.
-                      exclude: ["Reddit"],
-                      settings: {
-                        Instagram: {
-                          access_token: process.env.INSTAGRAM_ACCESS_TOKEN,
-                        }
-                      }
-                    },
-                  },
-                },
-              ],
+              providers: {
+                // Important to exclude providers
+                // that adds js to the page.
+                // If you do not need them.
+                exclude: ["Reddit"],
+                settings: {
+                  Instagram: {
+                    access_token: process.env.INSTAGRAM_ACCESS_TOKEN,
+                  }
+                }
+              },
             },
           },
           {
